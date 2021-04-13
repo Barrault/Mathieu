ac5749caba8fa55b42d3943fdee766a999128014
BARRAULT <mathieu.barrault9@gmail.com>
2021-04-13 16:22:45 +0200

    Jour 2

---
+++ Jour 2/Salaire/Salaire.js
@@ -0,0 +1,23 @@
+'use strict'; 
+
+Calculer le salaire d'une personne sur base de son salaire brut en décomptant les frais suivants: 
+
+Impôts sur le revenu : 18% 
+
+Assurance employé : 7% 
+
+Régime de pensions du Canada : 5% 
+
+Les personnes peuvent recevoir comme supplément un bonus de 100$ ou une allocation de 150$. 
+
+ 
+
+Il est possible de recevoir des réductions sur les impôts sur le revenu sous certaines conditions : 
+
+ 
+
+Si le travailleur est une femme, elle reçoit 2% de réduction. 
+
+Si le travailleur a 3 personnes à charge, il reçoit 1% de réduction. 
+
+Si le travailleur a 4 personnes à charge, il reçoit 2% de réduction. 
