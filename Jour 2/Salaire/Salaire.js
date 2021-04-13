d8c01ab746f5f4db82884b8ff88772d8a9ca9431
BARRAULT <mathieu.barrault9@gmail.com>
2021-04-13 17:25:50 +0200

    Salaire

--- Jour 2/Salaire/Salaire.js
+++ Jour 2/Salaire/Salaire.js
@@ -1,36 +1,48 @@
 'use strict'; 
 
-function personne(salaireBrut, sexe, nbPersonnesACharge){
+function personne(name, salaire, sexe, nbPersonnesACharge){
   
+  this.name = name;
   this.salaire = salaire;
   this.sexe = sexe;
   this.nbPersonnesACharge = nbPersonnesACharge;
 
   this.impotsAPayer = function(){
-    let taux = 0.18;
+    var taux = 18;
     if(this.sexe === "F"){
-      taux-=0.02;
+      taux-=2;
     }
     switch(nbPersonnesACharge){
       case 3:
-        taux -= 0.01;
+        taux = taux - 1;
       break;
       case 4:
-        taux -= 0.02;
+        taux = taux - 2;
     }
-    return this.salaire*taux;
+    console.log("Le taux de " + this.name + " est : " + taux + "%");
+    console.log("l'impot a payer de " + this.name + " est : " + this.salaire*taux/100);
+    return this.salaire*taux/100;
   }
 
   this.assuranceApayer = function(){
-    return this.salaire*0.07;
+    console.log("l'assurance a payer de " + this.name + " est : " + this.salaire*7/100);
+    return this.salaire*7/100;
   }
 
   this.pensionAPayer = function(){
-    return this.salaire*0.05;
+    console.log("la pension a payer de " + this.name + " est : " + this.salaire*5/100);
+    return this.salaire*5/100;
   }
 
   this.salaireNet = function(){
-    return this.salaire - this.impotsAPayer - this.assuranceApayer - this.pensionAPayer;
+    return this.salaire - this.impotsAPayer() - this.assuranceApayer(); - this.pensionAPayer();
   }
 
 }
+
+let sophie = new personne("sophie", 3000, "F", 4);
+let mathieu = new personne("mathieu", 2500, "M", 0);
+let Tom = new personne("Tom", 2000, "M", 3)
+console.log(sophie.salaireNet());
+console.log(mathieu.salaireNet());
+console.log(Tom.salaireNet());
